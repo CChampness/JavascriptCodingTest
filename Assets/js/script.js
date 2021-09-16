ccd217599dc834bcf8b17b711cf66bd0defeeb0b
CChampness <87551272+CChampness@users.noreply.github.com>
2021-09-16 17:43:19 -0400

    Added timer and data structure for list of Q & A

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -21,3 +21,50 @@ var questionList = [
         "Answer": "a"
        }
 ];
+
+var Question = {
+    "Question": "What is an element?",
+     "a": "Most general base class from which all element objects inherit",
+     "b": "tbd",
+     "c": "tbd",
+     "d": "tbd",
+     "Answer": "a"
+};
+
+function refreshQuizItem (ndx) {
+  var quizItem = questionList[ndx];
+  console.log(quizItem.Question);
+  var q = document.querySelector("h3");
+  console.log(q);
+  q.textContent = quizItem.Question;
+  var answerList = document.querySelectorAll("li");
+  answerList[0].textContent = "1. " + quizItem.a;
+  answerList[1].textContent = "2. " + quizItem.b;
+  answerList[2].textContent = "3. " + quizItem.c;
+  answerList[3].textContent = "4. " + quizItem.d;
+
+}
+
+for (var i = 0; i < questionList.length; i++) {
+  refreshQuizItem(i);
+}
+
+var h4 = document.querySelector("h4");
+
+function setTime(secondsLeft) {
+  h4.textContent = secondsLeft;
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    h4.textContent = secondsLeft;
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      h4.textContent = "EXPIRED";
+    }
+  }, 1000);
+}
+
+setTime(10);
