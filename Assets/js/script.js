3b90dc527e948b0bd9b000a50dd23e690a9df5c3
CChampness <87551272+CChampness@users.noreply.github.com>
2021-09-16 13:52:51 -0400

    giving the page its mockup format

---
+++ Assets/js/script.js
@@ -0,0 +1,23 @@
+var questionList = [
+    {"Question": "What is an element?",
+     "a": "Most general base class from which all element objects inherit",
+     "b": "tbd",
+     "c": "tbd",
+     "d": "tbd",
+     "Answer": "a"
+    },
+    {"Question": "What is a method?",
+     "a": "Function which an object makes available to its callers",
+     "b": "tbd",
+     "c": "tbd",
+     "d": "tbd",
+     "Answer": "a"
+    },
+    {"Question": "What is a property?",
+        "a": "Values associated with a JavaScript object",
+        "b": "tbd",
+        "c": "tbd",
+        "d": "tbd",
+        "Answer": "a"
+       }
+];
