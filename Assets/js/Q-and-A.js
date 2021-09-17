a6ed7f2d7b78ef3c8bc3cc96b101b8cd82f00cf1
CChampness <87551272+CChampness@users.noreply.github.com>
2021-09-17 15:53:16 -0400

    Added timer processing

---
+++ Assets/js/Q-and-A.js
@@ -0,0 +1,82 @@
+var questionList = [
+  {"Question": "What is an element?",
+   "a": "Most general base class from which all element objects inherit",
+   "b": "tbd",
+   "c": "tbd",
+   "d": "tbd",
+   "Answer": "a"
+  },
+  {"Question": "What is a method?",
+   "a": "tbd",
+   "b": "Function which an object makes available to its callers",
+   "c": "tbd",
+   "d": "tbd",
+   "Answer": "b"
+  },
+  {"Question": "What is a property?",
+   "a": "tbd",
+   "b": "tbd",
+   "c": "tbd",
+   "d": "Values associated with a JavaScript object",
+   "Answer": "d"
+  },
+  {
+   "Question": "Commonly use data types DO NOT include _____",
+   "a": "strings",
+   "b": "booleans",
+   "c": "alerts",
+   "d": "numbers",
+   "Answer": "c"
+  },
+  {
+   "Question": "The condition in an if/else statement is enclosed within ______",
+   "a": "curly brackets",
+   "b": "parentheses",
+   "c": "square brackets",
+   "d": "quotes",
+   "Answer": "b"
+  },
+  {
+   "Question": "A very useful tool used during development and debugging for printing content to the debugger is ______",
+   "a": "javascript",
+   "b": "for loops",
+   "c": "console.log",
+   "d": "terminal/bash",
+   "Answer": "c"
+  },
+  {
+   "Question": "String values must be enclose within ______ ",
+   "a": "curly brackets",
+   "b": "quotes",
+   "c": "parentheses",
+   "d": "commas",
+   "Answer": "b"
+  },
+  {
+   "Question": "Arrays in JavaScript can be used to store ______",
+   "a": "numbers and strings",
+   "b": "booleans",
+   "c": "others arrays",
+   "d": "all of the above",
+   "Answer": "d"
+  }
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,70 +1,83 @@
-var questionList = [
-    {"Question": "What is an element?",
-     "a": "Most general base class from which all element objects inherit",
-     "b": "tbd",
-     "c": "tbd",
-     "d": "tbd",
-     "Answer": "a"
-    },
-    {"Question": "What is a method?",
-     "a": "Function which an object makes available to its callers",
-     "b": "tbd",
-     "c": "tbd",
-     "d": "tbd",
-     "Answer": "a"
-    },
-    {"Question": "What is a property?",
-        "a": "Values associated with a JavaScript object",
-        "b": "tbd",
-        "c": "tbd",
-        "d": "tbd",
-        "Answer": "a"
-       }
-];
 
-var Question = {
-    "Question": "What is an element?",
-     "a": "Most general base class from which all element objects inherit",
-     "b": "tbd",
-     "c": "tbd",
-     "d": "tbd",
-     "Answer": "a"
-};
+var score = document.querySelector("#scoreboard").textContent;
+var numList = document.querySelectorAll("#num");
+var textList = document.querySelectorAll("#text");
+var q = document.querySelector("h3");
 
 function refreshQuizItem (ndx) {
   var quizItem = questionList[ndx];
-  console.log(quizItem.Question);
   var q = document.querySelector("h3");
-  console.log(q);
   q.textContent = quizItem.Question;
-  var answerList = document.querySelectorAll("li");
-  answerList[0].textContent = "1. " + quizItem.a;
-  answerList[1].textContent = "2. " + quizItem.b;
-  answerList[2].textContent = "3. " + quizItem.c;
-  answerList[3].textContent = "4. " + quizItem.d;
-
+  numList[0].textContent = "1. ";
+  numList[1].textContent = "2. ";
+  numList[2].textContent = "3. ";
+  numList[3].textContent = "4. ";
+  textList[0].textContent = quizItem.a;
+  textList[1].textContent = quizItem.b;
+  textList[2].textContent = quizItem.c;
+  textList[3].textContent = quizItem.d;
 }
 
-for (var i = 0; i < questionList.length; i++) {
-  refreshQuizItem(i);
-}
+var quizNdx = 0;
+var secondsLeft = 60
+var gameOn = false;
 
 var h4 = document.querySelector("h4");
+function gameOver() {
+  gameOn = false;
+  h4.textContent = "GAME OVER";
+  secondsLeft = 0;
+}
 
-function setTime(secondsLeft) {
+function setTime() {
   h4.textContent = secondsLeft;
   // Sets interval in variable
   var timerInterval = setInterval(function() {
-    secondsLeft--;
-    h4.textContent = secondsLeft;
-
-    if(secondsLeft === 0) {
-      // Stops execution of action at set interval
-      clearInterval(timerInterval);
-      // Calls function to create and append image
-      h4.textContent = "EXPIRED";
-    }
-  }, 1000);
+  secondsLeft--;
+  h4.textContent = secondsLeft;
+ 
+  if(secondsLeft <= 0) {
+    // Stops execution of action at set interval
+    clearInterval(timerInterval);
+    // Calls function to end the game
+    gameOver();
+  }}, 1000);
 }
 
-setTime(10);
+function scoreAnswer(key, ndx) {
+  if (key === questionList[ndx].Answer) {
+    score++;
+    document.querySelector("#scoreboard").textContent = score;
+  } else {
+    secondsLeft -= 10;
+  }
+  quizNdx++;
+  if (quizNdx == questionList.length) {
+    gameOver();
+  } else {
+    refreshQuizItem(quizNdx);
+  }
+}
+
+document.addEventListener('keydown', catchKey);
+
+function catchKey(e) {
+  if (!gameOn) {
+    return;
+  }
+  var keyCode = `${e.code}`;
+  var key = keyCode[5];
+  if (key === "1") {
+    scoreAnswer("a", quizNdx);
+  } else if (key === "2") {
+    scoreAnswer("b", quizNdx);
+  } else if (key === "3") {
+    scoreAnswer("c", quizNdx);
+  } else if (key === "4") {
+    scoreAnswer("d", quizNdx);
+  }
+}
+
+gameOn = true;
+refreshQuizItem(quizNdx);
+setTime(secondsLeft);
